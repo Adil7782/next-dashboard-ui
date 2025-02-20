9266dd4ccc1ad36d940fc900acb77b781979fea5
Your Name <adilsaaly@gmail.com>
2025-02-20 22:46:42 +0530

    student list done

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,7 +1,7 @@
 import Pagination from '@/app/components/Pagination'
 import Table from '@/app/components/Table'
 import TableSearch from '@/app/components/TableSearch'
-import { role, teachersData } from '@/lib/data'
+import { role, studentsData, teachersData } from '@/lib/data'
 import { headers } from 'next/headers'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -13,20 +13,16 @@ const columns = [
         accessor:"info"
     },
     {
-        header:"Teacher Id",
-        accessor:"teacherId",
+        header:"Student Id",
+        accessor:"studentId",
         className :"hidden md:table-cell"
     },
     {
-        header:"Subject",
-        accessor:"subject",
+        header:"Grade",
+        accessor:"grade",
         className :"hidden md:table-cell"
     },
-    {
-        header:"Classes",
-        accessor:"classes",
-        className :"hidden md:table-cell"
-    },
+  
     {
         header:"Phone",
         accessor:"phone",
@@ -43,21 +39,21 @@ const columns = [
     
 ]
 
-type teachers ={
+type Student ={
     id:number;
-    teacherId:string;
+    studentId:string;
     email?:string;
     name:string;
     photo:string;
-    phone:string;
-    subjects:string [];
-    classes:string [];
+    phone?:string;
+    grade:number ;
+    class:string;
     address:string
 }
 
-const TeachersList = () => {
+const StudentList = () => {
     
-    const renderRow =(item:teachers) =>{
+    const renderRow =(item:Student) =>{
 return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'>
           <td className='flex items-center p-3 gap-3'>
             <Image
@@ -70,18 +66,18 @@ return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 t
 
             <div className="flex flex-col">
               <h1 className="font-semibold">{item.name}</h1>
-              <p className="text-xs text-gray-500">{item.email}</p>
+              <p className="text-xs text-gray-500">{item.class}</p>
             </div>
           </td>
 
-          <td className="hidden md:table-cell text-sm">{item.teacherId} </td>
-          <td className="hidden md:table-cell text-sm">{item.subjects.join(",")}</td>
-          <td className="hidden md:table-cell text-sm">{item.classes.join(",")} </td>
+          <td className="hidden md:table-cell text-sm">{item.studentId} </td>
+          <td className="hidden md:table-cell text-sm">{item.grade}</td>
+          
           <td className="hidden md:table-cell text-sm"> {item.phone} </td>
           <td className="hidden md:table-cell text-sm"> {item.address} </td>
           <td>
             <div className="flex gap-2 items-center">
-              <Link href={`list/teachers/${item.id}`}>
+              <Link href={`list/students/${item.id}`}>
                 <button className="w-7 h-7 rounded-full flex items-center justify-center bg-lamaSky">
                   <Image src={"/view.png"} alt="" width={16} height={16} />
                 </button>
@@ -102,7 +98,7 @@ return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 t
     <>
     <div className='bg-white  mx-4 p-4  flex-1 rounded-xl'>
         <div className='flex justify-between items-center'>
-        <h1 className='text-lg  hidden md:block font-semibold '>All teachers</h1>
+        <h1 className='text-lg  hidden md:block font-semibold '>All Students</h1>
         <div className='flex flex-col md:flex-row items-center gap-8 w-full md:w-auto'>
         <TableSearch/>
         <div className='flex gap-4'>
@@ -115,8 +111,8 @@ return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 t
 
             </button>
             {
-              role === "admin" && 
-              <button className='bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex '>
+                role === "admin" && 
+                <button className='bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex '>
         <Image src="/plus.png" alt='' width={14} height={14}/>
 
             </button>}
@@ -124,7 +120,7 @@ return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 t
         </div>
         </div>
         <div>
-        <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+        <Table columns={columns} renderRow={renderRow} data={studentsData}/>
         </div>
       
         <Pagination/>
@@ -134,4 +130,4 @@ return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 t
   )
 }
 
-export default TeachersList
+export default StudentList
