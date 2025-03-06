c0efdb213149a09020e72e34690970d502e59814
Your Name <adilsaaly@gmail.com>
2025-03-06 11:34:32 +0530

    pagination done

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -3,6 +3,9 @@ import Pagination from '@/app/components/Pagination'
 import Table from '@/app/components/Table'
 import TableSearch from '@/app/components/TableSearch'
 import { role, teachersData } from '@/lib/data'
+import { db } from '@/lib/db'
+import { ITEM_PER_PAGE } from '@/lib/page'
+import { Class, Subject, Teacher } from '@prisma/client'
 import { headers } from 'next/headers'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -44,102 +47,125 @@ const columns = [
     
 ]
 
-type teachers ={
-    id:number;
-    teacherId:string;
-    email?:string;
-    name:string;
-    photo:string;
-    phone:string;
-    subjects:string [];
-    classes:string [];
-    address:string
+type TeacherType= Teacher & {subjects:Subject []} & {classes : Class[]}
+
+
+const renderRow =(item:TeacherType) =>{
+return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'>
+      <td className='flex items-center p-3 gap-3'>
+        <Image
+          src={item.img || "/avatar.png" }
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block  w-10 h-10 rounded-full object-cover"
+        />
+
+        <div className="flex flex-col">
+          <h1 className="font-semibold">{item.name}</h1>
+          <p className="text-xs text-gray-500">{item.email}</p>
+        </div>
+      </td>
+
+      <td className="hidden md:table-cell text-sm">{item.username} </td>
+      <td className="hidden md:table-cell text-sm">{item.subjects.map((sub)=>sub.name).join(",")}</td>
+      <td className="hidden md:table-cell text-sm">{item.classes.map((cls)=>cls.name).join(",")} </td>
+      <td className="hidden md:table-cell text-sm"> {item.phone} </td>
+      <td className="hidden md:table-cell text-sm"> {item.address} </td>
+      <td>
+        <div className="flex gap-2 items-center">
+          <Link href={`teachers/${item.id}`}>
+            <button className="w-7 h-7 rounded-full flex items-center justify-center bg-lamaSky">
+              <Image src={"/view.png"} alt="" width={16} height={16} />
+            </button>
+          </Link>
+
+          { role === "admin" && 
+            // <button className="w-7 h-7 rounded-full flex items-center justify-center bg-lamaPurple">
+            //   <Image src={"/delete.png"} alt="" width={16} height={16} />
+            // </button>
+
+          <FormComponent type='delete' table='teacher' id={item.id} />
+
+          }
+        </div>
+      </td>
+    </tr>)
 }
 
-const TeachersList = () => {
-    
-    const renderRow =(item:teachers) =>{
-return( <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'>
-          <td className='flex items-center p-3 gap-3'>
-            <Image
-              src={item.photo}
-              alt=""
-              width={40}
-              height={40}
-              className="md:hidden xl:block  w-10 h-10 rounded-full object-cover"
-            />
+const TeachersList = async ({  searchParams}:{searchParams: {[key:string]:string};
+}) => {
 
-            <div className="flex flex-col">
-              <h1 className="font-semibold">{item.name}</h1>
-              <p className="text-xs text-gray-500">{item.email}</p>
-            </div>
-          </td>
+console.log(searchParams)
+  const {page,...otherParams} = searchParams
 
-          <td className="hidden md:table-cell text-sm">{item.teacherId} </td>
-          <td className="hidden md:table-cell text-sm">{item.subjects.join(",")}</td>
-          <td className="hidden md:table-cell text-sm">{item.classes.join(",")} </td>
-          <td className="hidden md:table-cell text-sm"> {item.phone} </td>
-          <td className="hidden md:table-cell text-sm"> {item.address} </td>
-          <td>
-            <div className="flex gap-2 items-center">
-              <Link href={`teachers/${item.id}`}>
-                <button className="w-7 h-7 rounded-full flex items-center justify-center bg-lamaSky">
-                  <Image src={"/view.png"} alt="" width={16} height={16} />
-                </button>
-              </Link>
+  const p = page ? Number(page) : 1 // sets the default to 1st page
 
-              { role === "admin" && 
-                // <button className="w-7 h-7 rounded-full flex items-center justify-center bg-lamaPurple">
-                //   <Image src={"/delete.png"} alt="" width={16} height={16} />
-                // </button>
+  // const teachers = await db.teacher.findMany({
+  //   include: {
+  //     subjects: true,
+  //     classes: true,
+  //   },
+  //   take: 5,
+  //   skip : 5*(p-1)
+  // });
 
-              <FormComponent type='delete' table='teacher' id={item.id} />
-
-              }
-            </div>
-          </td>
-        </tr>)
-    }
+  // const count = await db.teacher.count()
+  
+  
+  const [teachers,count] = await db.$transaction(
+    [
+      db.teacher.findMany({
+        include: {
+          subjects: true,
+          classes: true,
+        },
+        take: ITEM_PER_PAGE,
+        skip : ITEM_PER_PAGE*(p-1)  // think the ipp is 10. so 10 to our page think its 2 -1 = 10 so it skips first 10
+      }),
+      db.teacher.count()
+      
+    ]
+  )
+  
+  // console.log(count)
 
 
   return (
     <>
-    <div className='bg-white  mx-4 p-4  flex-1 rounded-xl'>
-        <div className='flex justify-between items-center'>
-        <h1 className='text-lg  hidden md:block font-semibold '>All Teachers</h1>
-        <div className='flex flex-col md:flex-row items-center gap-8 w-full md:w-auto'>
-        <TableSearch/>
-        <div className='flex gap-4'>
-            <button className='bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex '>
-        <Image src="/filter.png" alt='' width={14} height={14}/>
+      <div className="bg-white  mx-4 p-4  flex-1 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h1 className="text-lg  hidden md:block font-semibold ">
+            All Teachers
+          </h1>
+          <div className="flex flex-col md:flex-row items-center gap-8 w-full md:w-auto">
+            <TableSearch />
+            <div className="flex gap-4">
+              <button className="bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex ">
+                <Image src="/filter.png" alt="" width={14} height={14} />
+              </button>
+              <button className="bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex ">
+                <Image src="/sort.png" alt="" width={14} height={14} />
+              </button>
+              {role === "admin" && (
+                //       <button className='bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex '>
+                // <Image src="/plus.png" alt='' width={14} height={14}/>
 
-            </button>
-            <button className='bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex '>
-        <Image src="/sort.png" alt='' width={14} height={14}/>
+                //     </button>
 
-            </button>
-            {
-              role === "admin" && 
-        //       <button className='bg-lamaYellow items-center justify-center h-8 w-8 rounded-full flex '>
-        // <Image src="/plus.png" alt='' width={14} height={14}/>
-
-        //     </button>
-            
-              <FormComponent type='create' table='teacher'  />
-
-            }
-        </div>
-        </div>
+                <FormComponent type="create" table="teacher" />
+              )}
+            </div>
+          </div>
         </div>
         <div>
-        <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+          <Table columns={columns} renderRow={renderRow} data={teachers} />
         </div>
-      
-        <Pagination/>
-        
-    </div>
+
+        <Pagination page={p} count={count} />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default TeachersList
--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,19 +1,45 @@
+"use client"
+import { ITEM_PER_PAGE } from '@/lib/page'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({page,count} : {page:number,count:number}) => {
+
+  const router = useRouter();
+
+  const changePage = (newPage:number) =>{
+    const params = new URLSearchParams(window.location.search)
+    params.set("page",newPage.toString())
+    router.push(`${window.location.pathname}?${params}`)
+  }
+
+  const hasPrevious = ITEM_PER_PAGE* (page-1) > 0 ;
+  const hasNext = ITEM_PER_PAGE* (page-1) +ITEM_PER_PAGE < count ;
+
   return (
     <div className=' p-4 flex justify-between items-center text-gray-500'>
-    <button disabled  className='py-2 my-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'>Preveious</button>
+    <button disabled = {!hasPrevious}   className='py-2 my-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed' onClick={()=>changePage(page-1)}>Preveious</button>
    
     <div className='flex gap-2 justify-center items-center'>
-    <button className='px-2 rounded-lg bg-lamaSky'>1</button>
-    <button className='p-2 '>2</button>
-    <button className='p-2 '>3</button>
-    ...
-    <button className='p-2 '>10</button>
+
+    {
+      Array.from({
+        length:Math.ceil(count/ ITEM_PER_PAGE)}
+        , (_,index) =>
+          { 
+            const pageIndex = index+1
+          return (
+          
+          <button key={index} className={`px-2 rounded-lg ${page === pageIndex ? "bg-lamaSky" :"" } `} onClick={()=>changePage(pageIndex)}>{pageIndex}</button>
+        )}
+      )
+    }
+
     </div>
 
-    <button   className='py-2 my-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed'>Next</button>
+    <button  
+    disabled={!hasNext}
+     className='py-2 my-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed' onClick={()=>changePage(page+1)}>Next</button>
 
     </div>
   )
